667a9ab311ebb065c56a336157cd96091d922daa
Rkonatam <konatamrohit@gmail.com>
2024-06-29 10:00:00 -0700

    added changes to System file

--- System.java
+++ System.java
@@ -1,16 +1,44 @@
 public class System {
-    private VitalsData recordedVitalsData;
-    private PatientDetails patientDetails;
-    private List<Prescription> prescriptions;
-    private List<Medication> prescribedMedications;
-    private ExaminationFindings examinationFindings;
-    private List<Message> messages;
+    // Private fields to store different aspects of the system's data
+    private VitalsData recordedVitalsData; // Stores recorded vitals data of patients
+    private PatientDetails patientDetails; // Stores patient details
+    private List<Prescription> prescriptions; // Stores a list of prescriptions
+    private List<Medication> prescribedMedications; // Stores a list of prescribed medications
+    private ExaminationFindings examinationFindings; // Stores examination findings
+    private List<Message> messages; // Stores messages
 
-    public void storeRecordedVitalsData(VitalsData data) { }
-    public void providePatientDetails(PatientDetails data) { }
-    public List<Prescription> allowViewPrescriptions() { return null; }
-    public void storePrescribedMedications(List<Medication> medications) { }
-    public void storeExaminationFindings(ExaminationFindings findings) { }
-    public void storeDeliverMessages(List<Message> messages) { }
-    public List<Message> allowReceivingMessages() { return null; }
+    // Method to store recorded vitals data
+    public void storeRecordedVitalsData(VitalsData data) {
+        this.recordedVitalsData = data;
+    }
+
+    // Method to provide patient details
+    public void providePatientDetails(PatientDetails data) {
+        this.patientDetails = data;
+    }
+
+    // Method to allow viewing of prescriptions
+    public List<Prescription> allowViewPrescriptions() {
+        return this.prescriptions;
+    }
+
+    // Method to store prescribed medications
+    public void storePrescribedMedications(List<Medication> medications) {
+        this.prescribedMedications = medications;
+    }
+
+    // Method to store examination findings
+    public void storeExaminationFindings(ExaminationFindings findings) {
+        this.examinationFindings = findings;
+    }
+
+    // Method to store and deliver messages
+    public void storeDeliverMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    // Method to allow receiving messages
+    public List<Message> allowReceivingMessages() {
+        return this.messages;
+    }
 }
