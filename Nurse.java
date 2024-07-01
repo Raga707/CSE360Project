a5f125ab3692325c4b35ea4e5e556768a3428011
Rkonatam <konatamrohit@gmail.com>
2024-07-01 10:00:00 -0700

    added changes to Nurse file

--- Nurse.java
+++ Nurse.java
@@ -1,7 +1,10 @@
+
+// getters and setters for this class
 public class Nurse {
     private Vitals patientVitals;
     private VitalsData vitalsData;
 
+    // now the constructor class
     // Constructor
     public Nurse() {
         this.patientVitals = new Vitals();
@@ -34,18 +37,39 @@ class Vitals {
     private String bloodPressure;
 
     // Getters and setters for the vitals
-    public String getWeight() { return weight; }
-    public void setWeight(String weight) { this.weight = weight; }
+    public String getWeight() {
+        return weight;
+    }
 
-    public String getHeight() { return height; }
-    public void setHeight(String height) { this.height = height; }
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
 
-    public String getBodyTemperature() { return bodyTemperature; }
-    public void setBodyTemperature(String bodyTemperature) { this.bodyTemperature = bodyTemperature; }
+    public String getHeight() {
+        return height;
+    }
 
-    public String getBloodPressure() { return bloodPressure; }
-    public void setBloodPressure(String bloodPressure) { this.bloodPressure = bloodPressure; }
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getBodyTemperature() {
+        return bodyTemperature;
+    }
+
+    public void setBodyTemperature(String bodyTemperature) {
+        this.bodyTemperature = bodyTemperature;
+    }
+
+    public String getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(String bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
 }
+// added vital data part
 
 class VitalsData {
     // Placeholder for the data storage logic
