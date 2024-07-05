c82c2e6f867a563085f1f23075824d99053ddbc3
Rkonatam <konatamrohit@gmail.com>
2024-07-05 10:00:00 -0700

    added changes to User file

--- User.java
+++ User.java
@@ -1,22 +1,28 @@
 public class User {
-    private String username;
-    private String password;
-    private String characterType;
+    // Private fields to store user information
+    private String username; // Stores the username of the user
+    private String password; // Stores the password of the user
+    private String characterType; // Stores the character type of the user
 
+    // Constructor to initialize a new User object with given username, password,
+    // and character type
     public User(String username, String password, String characterType) {
         this.username = username;
         this.password = password;
         this.characterType = characterType;
     }
 
+    // Getter method to retrieve the username
     public String getUsername() {
         return username;
     }
 
+    // Getter method to retrieve the password
     public String getPassword() {
         return password;
     }
 
+    // Getter method to retrieve the character type
     public String getCharacterType() {
         return characterType;
     }
